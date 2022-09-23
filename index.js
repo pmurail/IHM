ba0edbdd5425c502ee983463d10e6c5234905c0a
Pierre Murail (i2202537) <i2202537@univ-lemans.fr>
2022-09-23 15:07:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let input;
+
+function  init(){
+    
+    input = document.querySelectorAll(".input")
+                    .forEach(elt=>elt.addEventListener('click',verifChamp))
+    console.log(input);
+
+} 
+
+function verifChamp(){
+
+}
+
+function adresse(){
+
+}
+
+function  verifMail(){
+
+}
+
+function  verifMp(){
+
+}
+
+function verifMailConf(){
+
+}
+
+function verifMpConf(){
+
+}
